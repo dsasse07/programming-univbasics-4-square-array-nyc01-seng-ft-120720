32982226a69f291baf23339a66fe0c3308402003
Daniel Sasse <dsasse07@gmail.com>
2020-11-11 20:24:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,11 @@
+require 'pry'
 def square_array(array)
   # your code here 
   index = 0
   while index < array.length do 
     array[index] = array[index]**2
+    binding.pry
     index +=1 
+    binding.pry
   end
 end
