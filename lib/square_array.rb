ad9361912f386da913564dc0c24e0a222b1d40f3
Daniel Sasse <dsasse07@gmail.com>
2020-11-11 20:23:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here 
   index = 0
-  while array[index] do 
-    array.each { |i| i**2 }
+  while index < array.length do 
+    array[index] = array[index]**2
     index +=1 
   end
 end
